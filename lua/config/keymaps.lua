11b32648d0ba5073182abfc474f50c0fd412f69f
greed-d <deekshit.bti@gmail.com>
2025-02-13 12:36:23 +0545

    `add` : keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -9,6 +9,7 @@ del("n", "<leader>ft")
 del("n", "<leader>fm")
 del("n", "<leader>gg")
 del("n", "<leader>gG")
+del("n", "<leader>L")
 del("n", "<leader>gl")
 del("n", "<leader>sn")
 -- del("n", "<leader>h")
@@ -22,9 +23,11 @@ map({ "n", "v", "x" }, "<S-h>", "^", { desc = "Move to front of line" })
 map({ "n", "v", "x" }, "<S-l>", "$", { desc = "Move to end of line" })
 
 map("n", "<C-s>", "<cmd>w<CR>", { desc = "file save" })
-map("n", "<C-d>", "<C-d>zz", { desc = "file save" })
-map("n", "<C-u>", "<C-u>zz", { desc = "file save" })
+map("n", "<C-d>", "<C-d>zz", { desc = "Scroll half page down and center screen" })
+map("n", "<C-u>", "<C-u>zz", { desc = "Scroll half page up and center screen" })
 map("n", "<C-c>", "<cmd>%y+<CR>", { desc = "file copy whole" })
+map("n", "<CR>", "o<Esc>", { desc = "Add new line below" })
+map("n", "<S-CR>", "O<Esc>", { desc = "Add new line below" })
 
 map("n", "<Esc>", "<cmd>nohlsearch<CR>")
 map("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
@@ -43,25 +46,7 @@ end, { desc = "Terminal (Root Dir)" })
 -- Terminal Mappings
 map("t", "<M-i>", "<cmd>close<cr>", { desc = "Hide Terminal" })
 --
--- lazygit
-if vim.fn.executable("lazygit") == 1 then
-  map("n", "<leader>lg", function()
-    Snacks.lazygit({ cwd = LazyVim.root.git() })
-  end, { desc = "Lazygit (Root Dir)" })
-  map("n", "<leader>lG", function()
-    Snacks.lazygit()
-  end, { desc = "Lazygit (cwd)" })
-  map("n", "<leader>gf", function()
-    Snacks.lazygit.log_file()
-  end, { desc = "Lazygit Current File History" })
-  map("n", "<leader>gl", function()
-    Snacks.lazygit.log({ cwd = LazyVim.root.git() })
-  end, { desc = "Lazygit Log" })
-  map("n", "<leader>gL", function()
-    Snacks.lazygit.log()
-  end, { desc = "Lazygit Log (cwd)" })
-end
-
+--
 -- Git Stuff
 map("n", "<leader>gb", function()
   Snacks.git.blame_line()
@@ -82,29 +67,28 @@ end, { desc = "Delete Other Buffers" })
 
 map("t", "<C-x>", "<C-\\><C-N>", { desc = "terminal escape terminal mode" })
 
-map({ "n", "v", "i", "t" }, "<M-b>", function()
-  local terminal = require("nvterm.terminal")
-  local file = vim.fn.expand("%")
-  local sfile = vim.fn.expand("%:r")
-  local ft_cmds = {
-    sh = "bash " .. file,
-    rust = "cargo " .. file,
-    python = "python3 " .. file,
-    javascript = "node " .. file,
-    java = "javac " .. file .. " && java " .. sfile,
-    go = "go build && go run " .. file,
-    c = "g++ " .. file .. " -o " .. sfile .. " && ./" .. sfile,
-    cpp = "g++ " .. file .. " -o " .. sfile .. " && ./" .. sfile,
-    typescript = "deno compile " .. file .. " && deno run " .. file,
-  }
-
-  -- Save current buffer
-  local current_bufnr = vim.fn.bufnr("%")
-  vim.cmd("w")
-
-  -- Open terminal and run command
-  terminal.send(ft_cmds[vim.bo.ft], "float")
-
-  -- Focus back to terminal window
-  vim.cmd("wincmd p")
-end, { desc = "Build and Run file" })
+-- map("n", "<A-b>", function()
+--   require("nvchad.term").runner({
+--     id = "boo",
+--     pos = "float",
+--
+--     cmd = function()
+--       local file = vim.fn.expand("%")
+--       local sfile = vim.fn.expand("%:r")
+--
+--       local ft_cmds = {
+--         sh = "bash " .. file,
+--         rust = "cargo " .. file,
+--         python = "python3 " .. file,
+--         javascript = "node " .. file,
+--         java = "javac " .. file .. " && java " .. sfile,
+--         go = "go build && go run " .. file,
+--         c = "g++ " .. file .. " -o " .. sfile .. " && ./" .. sfile,
+--         cpp = "g++ " .. file .. " -o " .. sfile .. " && ./" .. sfile,
+--         typescript = "deno compile " .. file .. " && deno run " .. file,
+--       }
+--
+--       return ft_cmds[vim.bo.ft]
+--     end,
+--   })
+-- end)
