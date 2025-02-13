7f4d29836ac7634e8ea1e76f6f3b960d375b97db
greed-d <deekshit.bti@gmail.com>
2025-02-13 12:36:35 +0545

    `add` : hyprls for hyprland config

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,46 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+--
+local map = vim.keymap.set
+local opts = { noremap = true, silent = true }
+
+-- Hyprlang LSP
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+  pattern = { "*.hl", "hypr*.conf" },
+  callback = function(event)
+    print(string.format("starting hyprls for %s", vim.inspect(event)))
+    vim.lsp.start({
+      name = "hyprlang",
+      cmd = { "hyprls" },
+      root_dir = vim.fn.getcwd(),
+    })
+  end,
+})
+
+RefreshGuiFont = function()
+  vim.opt.guifont = string.format("%s:h%s", vim.g.gui_font_face, vim.g.gui_font_size)
+end
+
+ResizeGuiFont = function(delta)
+  vim.g.gui_font_size = vim.g.gui_font_size + delta
+  RefreshGuiFont()
+end
+
+ResetGuiFont = function()
+  vim.g.gui_font_size = vim.g.gui_font_default_size
+  RefreshGuiFont()
+end
+
+-- Call function on startup to set default value
+ResetGuiFont()
+
+map({ "n", "i" }, "<C-+>", function()
+  ResizeGuiFont(1)
+end, opts)
+map({ "n", "i" }, "<C-->", function()
+  ResizeGuiFont(-1)
+end, opts)
+map({ "n", "i" }, "<C-BS>", function()
+  ResetGuiFont()
+end, opts)
