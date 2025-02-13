72f1be07655fe483989321859e62132dd37cb6fd
greed-d <deekshit.bti@gmail.com>
2025-02-13 12:37:25 +0545

    `add` : disabled plugins

---
+++ lua/plugins/disabled.lua
@@ -0,0 +1,9 @@
+return {
+  { "iamcco/markdown-preview.nvim", enabled = false },
+  { "MeanderingProgrammer/render-markdown.nvim", enabled = true },
+  -- {
+  --   "akinsho/bufferline.nvim",
+  --   enabled = false,
+  -- },
+  { "lukas-reineke/headlines.nvim", enabled = false },
+}
