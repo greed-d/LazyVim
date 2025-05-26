87777e20d0297806bc147d1c672090395ec1d532
greed <greed-d>
2025-05-26 12:53:00 +0545

    `chore` : add more keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,14 +4,16 @@
 local del = vim.keymap.del
 local map = vim.keymap.set
 
-del("n", "<leader>ff")
-del("n", "<leader>ft")
-del("n", "<leader>fm")
+-- del("n", "<leader>ff")
+-- del("n", "<leader>ft")
+-- del("n", "<leader>fm")
+del("n", "<leader>gg")
 del("n", "<leader>gg")
 del("n", "<leader>gG")
 del("n", "<leader>L")
 del("n", "<leader>gl")
 del("n", "<leader>sn")
+-- del("n", "<leader>e")
 -- del("n", "<leader>h")
 del("n", "<S-h>")
 del("n", "<S-l>")
@@ -19,22 +21,24 @@ del("n", "<S-l>")
 -- Oil
 map("n", "-", "<cmd>Oil<CR>", { desc = "Toggle [O]il" })
 
-map({ "n", "v", "x" }, "<S-h>", "^", { desc = "Move to front of line" })
-map({ "n", "v", "x" }, "<S-l>", "$", { desc = "Move to end of line" })
+-- map({ "n", "v", "x" }, "<S-h>", "^", { desc = "Move to front of line" })
+-- map({ "n", "v", "x" }, "<S-l>", "$", { desc = "Move to end of line" })
 
 map("n", "<C-s>", "<cmd>w<CR>", { desc = "file save" })
 map("n", "<C-d>", "<C-d>zz", { desc = "Scroll half page down and center screen" })
+
+map("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror messages" })
 map("n", "<C-u>", "<C-u>zz", { desc = "Scroll half page up and center screen" })
 map("n", "<C-c>", "<cmd>%y+<CR>", { desc = "file copy whole" })
 map("n", "<CR>", "o<Esc>", { desc = "Add new line below" })
 map("n", "<S-CR>", "O<Esc>", { desc = "Add new line below" })
 
 map("n", "<Esc>", "<cmd>nohlsearch<CR>")
-map("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
-map("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
-map("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
-map("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
-map("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+-- map("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
+-- map("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
+-- map("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
+-- map("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
+-- map("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
 
 -- Terminal Stuff
 map("n", "<M-I>", function()
@@ -48,6 +52,7 @@ map("t", "<M-i>", "<cmd>close<cr>", { desc = "Hide Terminal" })
 --
 --
 -- Git Stuff
+--
 map("n", "<leader>gb", function()
   Snacks.git.blame_line()
 end, { desc = "Git Blame Line" })
@@ -58,14 +63,14 @@ end, { desc = "Git Browse (open)" })
 --Buffer Stuff
 map("n", "<S-Tab>", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
 map("n", "<Tab>", "<cmd>bnext<cr>", { desc = "Next Buffer" })
-map("n", "<leader>bk", function()
-  Snacks.bufdelete()
-end, { desc = "Delete Buffer" })
+-- map("n", "<leader>bk", function()
+--   Snacks.bufdelete()
+-- end, { desc = "Delete Buffer" })
 map("n", "<leader>bo", function()
   Snacks.bufdelete.other()
 end, { desc = "Delete Other Buffers" })
 
-map("t", "<C-x>", "<C-\\><C-N>", { desc = "terminal escape terminal mode" })
+map("t", "<Esc><Esc>", "<C-\\><C-N>", { desc = "terminal escape terminal mode" })
 
 -- map("n", "<A-b>", function()
 --   require("nvchad.term").runner({
