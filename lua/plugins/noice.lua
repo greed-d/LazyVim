8c36a80a9bd5721243e29a3bf7a85ba2f99db384
greed <greed-d>
2025-05-26 12:56:05 +0545

    `chore` : disable lsp progress for noice

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,10 +1,7 @@
 return {
   "folke/noice.nvim",
   event = "VeryLazy",
-  opts = {
-    -- add any options here
-  },
-  enabled = true,
+  opts = {},
   dependencies = {
     -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
     "MunifTanjim/nui.nvim",
@@ -22,6 +19,19 @@ return {
         long_message_to_split = true, -- long messages will be sent to a split
         inc_rename = false, -- enables an input dialog for inc-rename.nvim
         lsp_doc_border = true, -- add a border to hover docs and signature help
+      },
+      lsp = {
+        progress = {
+          enabled = false,
+          -- Lsp Progress is formatted using the builtins for lsp_progress. See config.format.builtin
+          -- See the section on formatting for more details on how to customize.
+          --- @type NoiceFormat|string
+          format = "lsp_progress",
+          --- @type NoiceFormat|string
+          format_done = "lsp_progress_done",
+          throttle = 1000 / 30, -- frequency to update lsp progress message
+          view = "mini",
+        },
       },
       views = {
         cmdline_popup = {
