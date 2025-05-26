c518bfb467c94db80e07bfa1d42ef50f2335ecb8
greed <greed-d>
2025-05-26 12:53:26 +0545

    `chore(lsp)` : change python lsp to basedpyright

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,11 +6,15 @@ local g = vim.g
 
 g.minipairs_disable = true
 g.snacks_animate = false
-
+-- Set to "basedpyright" to use basedpyright instead of pyright.
+vim.g.lazyvim_python_lsp = "basedpyright"
 --Vim Doge
 g.doge_doc_standard_python = "reST"
 vim.opt.pumblend = 0
 vim.opt.winblend = 0
+vim.opt.formatoptions:remove({ "c", "r", "o" }) -- Stop neovim from adding comments to new line
+-- Set to "ruff_lsp" to use the old LSP implementation version.
+vim.g.lazyvim_python_ruff = "ruff"
 
 -- NOTE: Configs after this are for NeoVide only
 
