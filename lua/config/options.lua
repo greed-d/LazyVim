5a98648f635deabda9820971d8f5f057c1c8b244
greed <greed-d>
2025-05-28 07:41:50 +0545

    `chore` : animation and font size changes for neovide

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -18,11 +18,11 @@ vim.g.lazyvim_python_ruff = "ruff"
 
 -- NOTE: Configs after this are for NeoVide only
 
-g.gui_font_default_size = 12
+g.gui_font_default_size = 13
 g.gui_font_size = vim.g.gui_font_default_size
 g.gui_font_face = "JetBrainsMono NF"
 
-g.neovide_cursor_animation_length = 0.03
+g.neovide_cursor_animation_length = 0.06
 
 g.neovide_text_gamma = 1.4
 g.neovide_text_contrast = 0.3
