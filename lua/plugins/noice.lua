2a3f345cb3fc892e4a47dc22ab4e539ac92271b8
greed <greed-d>
2025-05-28 07:43:37 +0545

    `chore` : change stuff

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -33,39 +33,39 @@ return {
           view = "mini",
         },
       },
-      views = {
-        cmdline_popup = {
-          position = {
-            row = 5,
-            col = "50%",
-          },
-          size = {
-            width = 60,
-            height = "auto",
-          },
-        },
-        popupmenu = {
-          relative = "editor",
-          position = {
-            row = 8,
-            col = "50%",
-          },
-          size = {
-            width = 60,
-            height = 10,
-          },
-          border = {
-            style = "rounded",
-            padding = { 0, 1 },
-          },
-          win_options = {
-            winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
-          },
-        },
-      },
-      cmdline = {
-        view = "cmdline_popup", -- cmdline_popup, cmdline
-      },
+      -- views = {
+      --   cmdline_popup = {
+      --     position = {
+      --       row = 5,
+      --       col = "50%",
+      --     },
+      --     size = {
+      --       width = 60,
+      --       height = "auto",
+      --     },
+      --   },
+      --   popupmenu = {
+      --     relative = "editor",
+      --     position = {
+      --       row = 8,
+      --       col = "50%",
+      --     },
+      --     size = {
+      --       width = 60,
+      --       height = 10,
+      --     },
+      --     border = {
+      --       style = "rounded",
+      --       padding = { 0, 1 },
+      --     },
+      --     win_options = {
+      --       winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
+      --     },
+      --   },
+      -- },
+      -- cmdline = {
+      --   view = "cmdline_popup", -- cmdline_popup, cmdline
+      -- },
     })
   end,
 }
