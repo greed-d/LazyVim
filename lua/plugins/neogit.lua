bd0721139db1e99d8b5173501da76ff89f67085f
greed-d <deekshit.bti@gmail.com>
2026-03-04 11:14:25 +0545

    chore(plugins) : neogit and stuff - add neogit - add gitsigns current line blame

---
+++ lua/plugins/neogit.lua
@@ -0,0 +1,26 @@
+return {
+  "NeogitOrg/neogit",
+  enabled = true,
+  keys = {
+    {
+      "<leader>gg",
+      "<cmd>Neogit<cr>",
+      desc = "Open neogit",
+    },
+    {
+      "<leader>gP",
+      "<cmd>Neogit push<cr>",
+      desc = "Neogit Push",
+    },
+    {
+      "<leader>gp",
+      "<cmd>Neogit pull<cr>",
+      desc = "Neogit pull",
+    },
+    {
+      "<leader>gc",
+      "<cmd>Neogit commit<cr>",
+      desc = "Neogit commit",
+    },
+  },
+}
